19d79540b73009e65159a2582c06fc4c9fff9bd1
yuerwen <2660955327@qq.com>
2022-05-05 00:03:36 +0800

    Initial commit

--- cases/in_class_environment/no_teacher_environment/no_students_environment/web_teacher_操作/teacher_operation.py
+++ cases/in_class_environment/no_teacher_environment/no_students_environment/web_teacher_操作/teacher_operation.py
@@ -9,36 +9,82 @@ from hytest import *
 from cfg.cfg import *
 from lib.api.yjyx_teacher_api import gs_teacher
 from lib.webUI.yjyx_webui_teacher_operation import teacher_operation
+from lib.api.yjyx_student_api import gs_student
 from lib.api.yjyx_class_api import gs_class
+import time
 
-class Case_tc005001:
-	name = '老师登录web系统 - tc005001'
+# 套件初始化方法
+def suite_setup():
+	# 班级id
+	cid = GSTORE['id']
+	STEP(1, '添加老师')
+	res_add_teacher = gs_teacher.add_teachers('web_yuer', '老师数学老师-文跃锐',
+	                                          SUBJECT_ID_HIGH_MATH, str(cid),
+	                                          '13553896530', 'yuerwen@123.com',
+	                                          '32092519830907899')
+	ret_add_teacher_json_obj = res_add_teacher.json()
 	
-	# 清除方法
-	def teardown(self):
-		gs_teacher.del_teachers(self.addTeacherId)
+	# ** 保存id，存到self中，self是实例对象都能访问到的东西
+	addTeacherId = ret_add_teacher_json_obj['id']
+	GSTORE['addTeacherId'] = addTeacherId
+	
+	teacher_operation.teacher_login('web_yuer')
+	
+# 清除方法
+def suite_teardown():
+	gs_teacher.del_teachers(GSTORE['addTeacherId'])
+	teacher_operation.close_chrome()  # 关闭浏览器
+	
+class Case_tc005001:
+	name = '老师登录1 - tc005001'
 	
 	def teststeps(self):
-		# 班级id
-		cid = GSTORE['id']
 		
 		# 测试步骤如下
-		STEP(1, '添加老师')
-		res_add_teacher = gs_teacher.add_teachers('web_yuer', '文跃锐',
-		                                          SUBJECT_ID_JUNIOR_MATH, str(cid),
-		                                          '13553896530', 'yuerwen@123.com',
-		                                          '32092519830907899')
-		ret_add_teacher_json_obj = res_add_teacher.json()
-		
-		# ** 保存id，存到self中，self是实例对象都能访问到的东西
-		self.addTeacherId = ret_add_teacher_json_obj['id']
-		CHECK_POINT('检查返回码信息', ret_add_teacher_json_obj['retcode'] == 0)
-		
-		STEP(2, '老师登录 web系统')
-		teacher_operation.teacher_login('web_yuer')
 		info_list = teacher_operation.get_home_page_info()
-		expected = ['白月学院00002', '文跃锐', '初中数学', '0', '0', '0']
+		INFO(info_list)
+		expected = ['白月学院00002', '老师数学老师-文跃锐', '高中数学', '0', '0', '0']
 		CHECK_POINT('检查首页信息',expected == info_list)
-		teacher_operation.close_chrome() # 关闭浏览器
 		
+		STEP(3,'点击 班级学生 菜单')
+		class_student_info = teacher_operation.get_class_student_info()
+		INFO(class_student_info)
+		expected = '该班级还没有学生注册'
+		CHECK_POINT('检查班级学生信息',class_student_info == expected)
+		
+
+class Case_tc005002:
+	name = '老师登录2 - tc005002'
+	
+	# 初始化方法
+	def setup(self):
+		cid = GSTORE['id']
+		result_add_student = gs_student.add_students(
+			'laoliu', '老六', SENIOR_THREE_GRADE_ID, cid, '1345166666')
+		GSTORE['student_id'] = result_add_student.json()['id']
+		
+	# 清除方法
+	def teardown(self):
+		gs_student.del_students(GSTORE['student_id'])
+	
+	def teststeps(self):
+		
+		# 测试步骤如下
+	
+		STEP(1, '检查首页信息')
+		info_list = teacher_operation.get_home_page_info()
+		INFO(info_list)
+		expected = ['白月学院00002', '老师数学老师-文跃锐', '高中数学', '0', '0', '0']
+		CHECK_POINT('检查首页信息', expected == info_list)
+		
+		STEP(2, '点击 班级学生 菜单')
+		teacher_operation.web_driver_refresh()
+		class_student_info = teacher_operation.get_class_student_info()
+		student_name = class_student_info.replace('\n', ' ').replace(' ', ',').split('|')[-1].split(',')[-4]
+		INFO(student_name)
+		expected = '老六'
+		CHECK_POINT('检查班级学生信息', student_name == expected)
+		
+		
+
 		
--- lib/webUI/yjyx_webui_teacher_operation.py
+++ lib/webUI/yjyx_webui_teacher_operation.py
@@ -10,6 +10,9 @@ from selenium.webdriver.common.by import By
 from cfg.cfg import *
 import time
 from hytest import *
+from selenium.webdriver.common.action_chains import ActionChains
+
+
 class TeacherOperation:
 	
 	# 教师登录
@@ -51,15 +54,56 @@ class TeacherOperation:
 		
 		return [info_element.text for info_element in info_elements]
 	
+	
+	def get_class_student_info(self):
+		
+		'''
+		获取学生班级信息
+		:return: getList_class_student_text 返回学生列表信息
+		'''
+		# 鼠标移动到此处
+		my_action = ActionChains(self.web_driver)
+		elems_main_menu_a = self.web_driver.find_elements(By.CSS_SELECTOR,
+		                                                  '.topbar-main .main-menu li > a')
+		my_action.move_to_element(elems_main_menu_a[-1]).perform()
+		
+		# 点击班级情况
+		self.web_driver.find_element(By.CSS_SELECTOR,'.main-menu .menu-title').click()
+		# 点击班级学生
+		getList_class_students = self.web_driver.find_elements(By.CSS_SELECTOR,
+		                                                       '.ng-scope .panel')
+		getList_class_students[-1].find_element(By.CSS_SELECTOR, '.panel-heading').click()
+	
+		# 等待加载列表信息
+		# 经验：不加载的情况下报错 StaleElementReferenceException
+		time.sleep(1)
+		try:
+			getList_class_student_text = getList_class_students[-1].find_element(By.CSS_SELECTOR,
+                                                             '.panel-body > .ng-scope').text
+		except:
+			print('加载不到列表信息')
+		
+		return getList_class_student_text
+		
+	
 	def close_chrome(self):
 		'''
 		关闭浏览器
-		:return:
+		:return:没有
 		'''
 		self.web_driver.close()
 		
+	def web_driver_refresh(self):
+		'''
+		刷新方法浏览器
+		:return: 没有
+		'''
+		self.web_driver.implicitly_wait(5)
+		self.web_driver.refresh()
+		
 teacher_operation = TeacherOperation()
 if __name__ == '__main__':
-	teacher_operation.teacher_login('yue')
-	infoList_text = teacher_operation.get_home_page_info()
-	print(infoList_text)
+	teacher_operation.teacher_login('jcyrss','sdfsdf5%')
+	time.sleep(2)
+	teacher_operation.get_class_student_info()
+	
